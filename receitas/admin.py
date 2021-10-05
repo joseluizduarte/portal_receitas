b0d1e286f587744cf065860944695db735aa8f18
joseluizduarte <jlduartejr@live.com>
2021-10-05 00:53:13 -0400

    Adicionado modelo inicial

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from receitas.models import Tag, Produto, Ingrediente, Receita
+
+admin.site.register(Tag)
+admin.site.register(Produto)
+admin.site.register(Ingrediente)
+admin.site.register(Receita)
--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    nome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nome
+
+
+class Ingrediente(models.Model):
+    quantidade = models.FloatField()
+    quantificador = models.CharField(max_length=20, blank=True)
+    produto_ingrediente = models.ForeignKey(Produto, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.quantidade} {self.quantificador} de {self.produto_ingrediente}'
+
+
+class Receita(models.Model):
+    nome = models.CharField(max_length=50)
+    tags = models.ManyToManyField(Tag)
+    ingrediente_receita = models.ManyToManyField(Ingrediente)
+    preparo = models.TextField()
+
+    def __str__(self):
+        return self.nome
